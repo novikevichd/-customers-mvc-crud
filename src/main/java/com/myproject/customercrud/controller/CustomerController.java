45b2cdddbb6de327c484c0ebe39bc487880c098e
Novikevich Dmitry <novikevichdm@gmail.com>
2020-12-26 22:49:00 +1200

    Created "add-new-customer" form and new Request mappings in CustomerController to save new customers in database

--- src/main/java/com/myproject/customercrud/controller/CustomerController.java
+++ src/main/java/com/myproject/customercrud/controller/CustomerController.java
@@ -6,20 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/customers")
-public class EmployeeController {
+public class CustomerController {
 
     CustomerDAO customerDAO;
 
     @Autowired
-    EmployeeController(CustomerDAO customerDAO) {
+    CustomerController(CustomerDAO customerDAO) {
         this.customerDAO = customerDAO;
     }
+
+
     @GetMapping("/list")
     public String listEmployees(Model theModel) {
 
@@ -28,6 +32,26 @@ public class EmployeeController {
         theModel.addAttribute("customers", customers);
 
         return "customers/customers-list";
+    }
+
+    @GetMapping("/showFormForAddCustomer")
+    public String showFormForAddCustomer(Model theModel) {
+
+        Customer theCustomer = new Customer();
+
+        theModel.addAttribute("customer", theCustomer);
+
+        return "customers/add-new-customer";
+
+    }
+
+    @PostMapping("/save")
+    public String saveCustomer(@ModelAttribute("customer") Customer theCustomer){
+
+        customerDAO.save(theCustomer);
+
+        return "redirect:/customers/list";
+
     }
 
 }
--- src/main/java/com/myproject/customercrud/entity/Customer.java
+++ src/main/java/com/myproject/customercrud/entity/Customer.java
@@ -8,7 +8,7 @@ public class Customer {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "first_name")
